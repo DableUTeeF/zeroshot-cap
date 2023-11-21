dc9c057c0c39a69d15af8f2d49dc325d26d536f0
DableUTeeF <palm22180@gmail.com>
2023-11-21 10:02:37 +0700

    Sort of work better

--- montecarlo.py
+++ montecarlo.py
@@ -6,6 +6,7 @@ from torch import nn
 import numpy as np
 import json
 import random
+from pythainlp import correct
 
 
 class Model:
@@ -54,6 +55,10 @@ class Model:
         return
 
 
+def get_wl():
+    return open('wordlist.txt').read().split('\n')
+
+
 def search(model,
            prompt='รูปของ<mask><mask><mask><mask><mask><mask><mask><mask>',
            gt='image of a sleeping girl amidst elegant swirling water',
@@ -61,6 +66,9 @@ def search(model,
            width=3,
            samples=100,
            scores=None,
+           chosen_index=None,
+           index_prop=0.5,
+           wordlist=get_wl(),
            ):
     if depth < 1:
         return scores
@@ -73,15 +81,21 @@ def search(model,
     m = model.lm_model(**prompts)
     x = m.logits.argsort(-1, True)
     mask = prompt_ids == model.mask_token_id
-    if np.random.rand() > 0.5 and mask.sum() > 0:
-        idx = np.random.randint(x.size(1))
+    if chosen_index is None:
+        if np.random.rand() > index_prop and mask.sum() > 0:
+            chosen_idx = torch.where(mask)[0].cpu().numpy()
+            idx = np.random.choice(chosen_idx)
+        else:
+            idx = np.random.randint(x.size(1))
     else:
-        chosen_idx = torch.where(mask)[0].cpu().numpy()
-        idx = np.random.choice(chosen_idx)
+        idx = chosen_index
     x = x[0, idx, :samples]
     temp_scores = []
     temp_prompts = []
     for s in range(samples):
+        temp_token = model.detokenize(x[s])
+        if temp_token not in wordlist:
+            continue
         temp_input_ids = prompts['input_ids'][0]
         temp_input_ids[idx] = x[s]
         temp_prompt = model.detokenize(temp_input_ids)
@@ -97,8 +111,8 @@ def search(model,
             np.random.shuffle(indice)
         else:
             indice = indice.cpu().numpy()
-            indice1 = indice[:samples//2]
-            indice2 = indice[samples//2:]
+            indice1 = indice[:samples // 2]
+            indice2 = indice[samples // 2:]
             np.random.shuffle(indice2)
             indice = np.concatenate((indice1, indice2))
 
@@ -106,24 +120,44 @@ def search(model,
         temp_prompt = temp_prompts[indice[w]]
         encoded_prompt = model.encode(temp_prompt)
         scores[temp_prompt] = util.dot_score(encoded_prompt, encoded_gt)[0][0].cpu().numpy().tolist()
-        scores = search(model, temp_prompt+'<mask>'*5, gt=gt, scores=scores, width=width, depth=depth-1, samples=samples)
+        scores = search(model, temp_prompt + '<mask>' * 5, gt=gt, scores=scores, width=width, depth=depth - 1, samples=samples)
     return scores
 
 
 if __name__ == '__main__':
     scoring_model = Model()
-    s = search(scoring_model, depth=7, width=3)
-    json.dump(s,
-              open('out/a.json', 'w'))
-    scores = []
-    texts = []
-    max_score = 0
-    max_text = ''
-    for k, v in s.items():
-        scores.append(v)
-        texts.append(k)
-        if v > max_score:
-            max_score = v
-            max_text = k
-    print(max_text, max_score)
-    print(s)
+    iters = []
+    prompts = [
+        'รูปของ<mask><mask><mask><mask><mask><mask><mask><mask>',
+        'รูปของ<mask><mask><mask><mask><mask><mask><mask><mask>',
+        'รูปของ<mask><mask><mask><mask><mask><mask><mask><mask>',
+        'รูปของ<mask><mask><mask><mask><mask><mask><mask><mask>',
+        'รูปของ<mask><mask><mask><mask><mask><mask><mask><mask>',
+    ]
+    s = {}
+    for i in range(3):
+        # explore
+        for j in range(5):
+            s = search(scoring_model, prompts[j] + "<mask>", depth=3, width=3, index_prop=0.5 if i == 0 else 1, scores=s)
+        ak = np.array(list(s.keys()))
+        av = torch.tensor(list(s.values()))
+        prompts = ak[av.argsort(-1, True)]
+
+        # fine_tune
+        for j in range(50):
+            if j*50 >= len(prompts):
+                break
+            prompt = prompts[j*50]  # todo: maybe use j*50 instead
+            pts = scoring_model.tokenize(prompt)
+            prompt_ids = pts['input_ids'][0]
+            for k in range(1, prompt_ids.size(0)):
+                s = search(scoring_model, prompt, depth=1, width=3, index_prop=1, scores=s, chosen_index=k)
+
+        ak = np.array(list(s.keys()))
+        av = torch.tensor(list(s.values()))
+        prompts = ak[av.argsort(-1, True)[:5]]
+        print(prompts)
+    json.dump(
+        s,
+        open('out/d.json', 'w')
+    )
